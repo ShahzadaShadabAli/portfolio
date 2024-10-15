a43f2b3ec68e97c400c1b6a61006a698c42f44da
ShahzadaShadabAli <shadabshahzada8@gmail.com>
2024-10-15 15:42:34 +0500

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" type="image/png" href="/boy.png" sizes="32x32" />
+      </head>
       <body className={inter.className}>
       <ThemeProvider
             attribute="class"
